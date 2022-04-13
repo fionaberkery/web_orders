82a704b0d7c4804d0471bf646506059b3fd6902a
Fiona <fiona_berkery@hotmail.com>
2022-04-13 14:02:16 +0100

    mvp completed

---
+++ models/order_list.py
@@ -0,0 +1,8 @@
+from models.order import *
+
+
+order1 = Order("Fiona", "22/03/22", 1, "Birdsong")
+order2 = Order("Adam", "03/05/2021", 1, "The Book Thief")
+order3 = Order("Dave", "17/03/2022", 1, "Twilight")
+
+orders = (order1, order2, order3)
