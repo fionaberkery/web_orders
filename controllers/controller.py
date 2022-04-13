53a4826897bb3988309eab2716221b49189ea9a9
Fiona <fiona_berkery@hotmail.com>
2022-04-13 14:55:56 +0100

    extension completed

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,3 +1,4 @@
+from code import interact
 from flask import render_template # ADDED
 from app import app
 from models.order_list import orders
@@ -5,3 +6,8 @@ from models.order_list import orders
 @app.route('/orders')
 def index():
     return render_template('index.html', title='Home', orders=orders)
+
+@app.route('/order/<index>')
+def order(index):
+    order = orders[int(index)]
+    return render_template('order.html', title='Single Order', order=order)
